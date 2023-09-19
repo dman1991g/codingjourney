27d0c77faffee05106e53ce481a09237427ba9fc
dman1991g <141966927+dman1991g@users.noreply.github.com>
2023-09-19 16:14:02 -0400

    Update myjavascript.js
    Added age calculator script

--- javascript/myjavascript.js
+++ javascript/myjavascript.js
@@ -18,3 +18,28 @@ window.onclick = function(event) {
   }
 } 
 
+const form = document.querySelector('form');
+const birthdateInput = document.querySelector('#birthdate');
+const ageOutput = document.querySelector('#age');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const birthdateString = birthdateInput.value;
+  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+
+  if(!dateRegex.test(birthdateString)) {
+    alert('Please enter a valid birthdate in MM/DD/YYYY format.');
+    return;
+  }
+
+  const birthdate = new Date(birthdateString);
+
+  const ageInMilliseconds = Date.now() - birthdate.getTime();
+  const ageDate = new Date(ageInMilliseconds);
+  const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+  ageOutput.textContent = `You are ${age} years old.`;
+});
+form.addEventListener('reset', function() {
+  ageOutput.textContent = null;
+})
