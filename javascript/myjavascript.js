b1e5134ceb87021f7ab3c72116bdb2434e005435
Juan Pablo Gutiérrez Restrepo <jpgrxx@gmail.com>
2023-09-20 14:54:27 -0500

    Fixed dark mode

--- javascript/myjavascript.js
+++ javascript/myjavascript.js
@@ -4,58 +4,56 @@ var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.onclick = function () {
+	modal.style.display = "block";
+};
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = function () {
+	modal.style.display = "none";
+};
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
 
-const form = document.querySelector('form');
-const birthdateInput = document.querySelector('#birthdate');
-const ageOutput = document.querySelector('#age');
+const form = document.querySelector("form");
+const birthdateInput = document.querySelector("#birthdate");
+const ageOutput = document.querySelector("#age");
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
+// The script stops running because the form is not found on the page.
 
-  const birthdateString = birthdateInput.value;
-  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+// form.addEventListener("submit", function (e) {
+// 	e.preventDefault();
 
-  if(!dateRegex.test(birthdateString)) {
-    alert('Please enter a valid birthdate in MM/DD/YYYY format.');
-    return;
-  }
+// 	const birthdateString = birthdateInput.value;
+// 	const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
 
-  const birthdate = new Date(birthdateString);
+// 	if (!dateRegex.test(birthdateString)) {
+// 		alert("Please enter a valid birthdate in MM/DD/YYYY format.");
+// 		return;
+// 	}
 
-  const ageInMilliseconds = Date.now() - birthdate.getTime();
-  const ageDate = new Date(ageInMilliseconds);
-  const age = Math.abs(ageDate.getUTCFullYear() - 1970);
- ageOutput.textContent = `You are ${age} years old.`;
-});
-form.addEventListener('reset', function() {
-  ageOutput.textContent = null;
-})
-  const body = document.querySelector('body');
-const modeToggle = document.getElementById('mode-toggle');
-const modeStatus = document.querySelector('.mode-status');
+// 	const birthdate = new Date(birthdateString);
+
+// 	const ageInMilliseconds = Date.now() - birthdate.getTime();
+// 	const ageDate = new Date(ageInMilliseconds);
+// 	const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+// 	ageOutput.textContent = `You are ${age} years old.`;
+// });
+// form.addEventListener("reset", function () {
+// 	ageOutput.textContent = null;
+// });
+const body = document.querySelector("body");
+const modeToggle = document.getElementById("mode-toggle");
+const modeStatus = document.querySelector(".mode-status");
 
 function toggleMode() {
-  body.classList.toggle('dark-mode');
+	body.classList.toggle("dark-mode");
 
-  const modeMessage = body.classList.contains('dark-mode') ?
-    'Dark Mode' 
-    : "Light Mode"
+	const modeMessage = body.classList.contains("dark-mode") ? "Dark Mode" : "Light Mode";
 
-  modeStatus.innerText = "Currently in " + modeMessage;
+	modeStatus.innerText = "Currently in " + modeMessage;
 }
-modeToggle.addEventListener('click', toggleMode);
-
-
+modeToggle.addEventListener("click", toggleMode);
