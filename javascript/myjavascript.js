1aae2cd40733cd1bba4059b22b1749076d73986d
dman1991g <141966927+dman1991g@users.noreply.github.com>
2023-10-09 10:47:40 -0400

    Update myjavascript.js
    Added script to validate the form on contact page

--- javascript/myjavascript.js
+++ javascript/myjavascript.js
@@ -57,3 +57,113 @@ function toggleMode() {
 	modeStatus.innerText = "Currently in " + modeMessage;
 }
 modeToggle.addEventListener("click", toggleMode);
+
+const form = document.getElementById('exampleForm')
+const submitButton = document.querySelector('.submit')
+const successMessage = document.getElementById('form-submitted-msg')
+const formElements = [ ...form.elements ]
+
+const allInputsValid = () => {
+  const valid = formElements.every((element) => {
+    if (element.nodeName === 'SELECT') {
+      return element.value !== 'Please select an option'
+    } else {
+      return element.checkValidity()
+    }
+  })
+
+
+  return valid
+}
+
+// Define a function to handle changes to any form element
+const handleChange = () => {
+  // Use the forEach() function to execute the provided function once for each element in the formElements array
+  formElements.forEach((element) => {
+    // If the element is invalid and is not a button, a select dropdown, a checkbox, or a radio button, style it with a red border and red text
+    if (!element.checkValidity()
+          && element.nodeName !== 'BUTTON'
+          && element.nodeName !== 'SELECT'  
+          && element.type !== 'checkbox'
+          && element.type !== 'radio'
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+      element.nextElementSibling.style.display = 'block'
+      element.previousElementSibling.style.color = 'red'
+    }
+
+    if (element.checkValidity()
+          && element.nodeName !== 'BUTTON'
+          && element.nodeName !== 'SELECT'
+          && element.type !== 'checkbox'
+          && element.type !== 'radio'
+    ) {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#CED4DA'
+      element.nextElementSibling.style.display = 'none'
+      element.previousElementSibling.style.color = '#212529'
+    }
+
+    if (!element.checkValidity()
+          && (element.type === 'checkbox'
+              || element.type === 'radio')
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+    }
+
+    if (element.checkValidity()
+          && (element.type === 'checkbox'
+              || element.type === 'radio')
+    ) {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#212529'
+    }
+
+    if (element.nodeName === 'SELECT'
+          && element.value === 'Please select an option'
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+      element.nextElementSibling.style.display = 'block'
+      element.previousElementSibling.style.color = 'red'
+    }
+
+    if (element.nodeName === 'SELECT'
+          && element.value !== 'Please select an option'
+    ) {
+      element.style.borderColor = '#CED4DA'
+      element.nextElementSibling.style.color = '#CED4DA'
+      element.nextElementSibling.style.display = 'none'
+      element.previousElementSibling.style.color = '#212529'
+    }
+  })
+
+  if (allInputsValid()) {
+    submitButton.removeAttribute('disabled', '')
+  } else {
+    submitButton.setAttribute('disabled', '')
+  }
+
+const handleSubmit = (e) => {
+  
+  e.preventDefault()
+
+  if (allInputsValid()) {
+    successMessage.style.display = 'block'
+    form.reset()
+    submitButton.setAttribute('disabled', '')
+
+    setTimeout(() => {
+      successMessage.style.display = 'none'
+    }, 3000)
+  }
+}
+
+formElements.forEach((element) => {
+  element.addEventListener('change', handleChange)
+})
+
+form.addEventListener('submit', (e) => handleSubmit(e))
+}
